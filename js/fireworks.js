c9e6b6e769aaa289446fc83842b2efe907986b2f
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2022-04-01 17:56:24 -0500

    Utilized iterator for inplace removal update

--- js/fireworks.js
+++ js/fireworks.js
@@ -1,7 +1,16 @@
+import * as Ag from "./AgCollections.js";
+
 const colors = ["red", "green", "blue", "violet", "orange", "yellow", "magenta"];
 const fireworkRadius = 2;
 
-/**  @type {HTMLCanvasElement} */
+const fireworks = new Ag.LinkedList();
+
+let explosion = 0;
+let doubleExplosion = 0;
+let tripleExplosion = 0;
+let recursiveExplosion = 0;
+
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("fireworkCanvas");
 const canvasContainer = document.getElementById("canvas-container");
 window.addEventListener("resize", canvasUpdate);
@@ -19,7 +28,6 @@ const c = canvas.getContext("2d");
 const target = document.getElementById("fireworkTarget");
 let isInView = false;
 document.addEventListener("scroll", scrollCheck);
-let fireworks = [];
 let anyDied = false;
 const explosionTicks = 60 * 4.5;
 
@@ -33,7 +41,7 @@ function mouseUpdate(e) {
     if (cy < rect.bottom && cy > rect.top) {
         const tx = cx - rect.left;
         const ty = cy - rect.top;
-        genExplosions(tx, ty, randomElement(colors));
+        genExplosions(tx, ty, randomElement(colors), true);
     }
 }
 class Explosion {
@@ -53,7 +61,7 @@ class Explosion {
         c.globalAlpha = this.ticks / explosionTicks;
         c.fillStyle = this.color;
         c.beginPath();
-        c.arc(this.x, this.y, this.radius, Math.PI * 2, 0, 0);
+        c.arc(this.x, this.y, this.radius, Math.PI * 2, 0);
         c.closePath();
         c.fill();
         c.restore();
@@ -99,7 +107,7 @@ class Firework {
         c.fillStyle = this.color;
         c.strokeStyle = "black";
         c.beginPath();
-        c.arc(this.x, this.y, this.radius, Math.PI * 2, 0, 0);
+        c.arc(this.x, this.y, this.radius, Math.PI * 2, 0);
         c.closePath();
         c.stroke();
         c.fill();
@@ -120,7 +128,7 @@ class Firework {
         if (Math.sqrt((this.x - this.ox) ** 2 + (this.y - this.oy) ** 2) > this.arcHeight) {
             this.isAlive = false;
             anyDied = true;
-            genExplosions(this.x, this.y, this.color);
+            genExplosions(this.x, this.y, this.color, true);
         }
     }
 }
@@ -137,26 +145,70 @@ function getMaxVY() {
     return -1 * window.innerHeight / (fireworkRadius * 60); 
 }
 
-function genExplosions(x, y, color) {
+const explosionDiv = document.getElementById("explosion");
+
+/**
+ * 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} color 
+ * @param {*} canGenerate if this can generate recursive calls. Default = false;
+ */
+function genExplosions(x, y, color, canGenerate = false) {
+    explosion++;
     const maxV = getMinV();
-    // This reduction algorithm attempts to create a circular
+    // random chance to generate another explosion at the same x,y
+    const doubleExplosionChance = 0.25;
+    const tripleExplosionChance = 0.05;
+    const recursiveExplosionChance = 0.005;
+    const recursivePropagation = 0.12;
+    const maxPropagations = 14;
+    const gen = Math.random();
+    const isRecursive = canGenerate && gen <= recursiveExplosionChance;
+
+
+    if (canGenerate) {
+
+        if (isRecursive) {
+            recursiveExplosion++;
+        } else {
+            if (gen <= doubleExplosionChance) {
+                doubleExplosion++;
+                genExplosions(x, y, randomElement(colors));
+            } else if (gen <= tripleExplosionChance) {
+                genExplosions(x, y, randomElement(colors));
+                genExplosions(x, y, randomElement(colors));
+                tripleExplosion++;
+            }
+        }
+    }
+
+    explosionDiv.innerText = `Explosions: ${explosion} Doubles: ${doubleExplosion} Triples: ${tripleExplosion}` +
+        "\n" + (recursiveExplosion < 1 ? "?: 0" : `Recursives: ${recursiveExplosion}`);
+
+    // The reduction algorithm attempts to create a circular
     // effect by reducing the "corners" of the random dist
     // of particles
     const reduceAmount = 0.65;
-    const reduceThreshold = ((maxV*0.25)**2 + (maxV*0.25)**2) * 0.55;
+    const reduceThreshold = ((maxV * 0.25) ** 2 + (maxV * 0.25) ** 2) * 0.55;
     let e;
     let rx;
     let ry;
+    let propagated = 0;
 
     for (let i = 0; i < 100; i++) {
         rx = Math.random() * (maxV * 0.25) * (Math.random() >= 0.5 ? 1 : -1);
         ry = Math.random() * (maxV * 0.25) * (Math.random() >= 0.5 ? 1 : -1);
-        if (rx**2 + ry**2 > reduceThreshold) {
+        if (rx ** 2 + ry ** 2 > reduceThreshold) {
             rx *= reduceAmount;
             ry *= reduceAmount;
         }
         e = new Explosion(x, y, rx, ry, color);
         fireworks.push(e);
+        if (isRecursive && propagated < maxPropagations && Math.random() < recursivePropagation) {
+            genExplosions(x + rx * maxV * 0.3, y + ry * maxV * 0.3, randomElement(colors));
+            propagated++;
+        }
     }
 }
 
@@ -165,7 +217,7 @@ function scrollCheck() {
         isInView = false;
         return;
     }
-    if (!isInView && fireworks.length == 0) {
+    if (!isInView && fireworks.size() == 0) {
         for (let i = 0; i < 8; i++) {
             let f = randFirework();
             fireworks.push(f);
@@ -193,18 +245,25 @@ function randomElement(list) {
     return list[i];
 }
 
-
 let lastTime;
+let lastPrintTime;
 function update(time) {
     if (lastTime === undefined) lastTime = time;
+    if (lastPrintTime === undefined) lastPrintTime = time;
     c.clearRect(0, 0, canvas.width, canvas.height);
-    for (let f of fireworks) {
-        f.tick(time - lastTime);
-        f.draw();
-    }
-    if (anyDied) {
-        fireworks = fireworks.filter(f => f.isAlive);
-        anyDied = false;
+    
+    // In place removal makes this much more efficient
+    const it = fireworks.iterator();
+    let f;
+
+    while (it.hasNext()) {
+        f = it.next();
+        if (!f.isAlive) {
+            it.remove();
+        } else {
+            f.tick(time - lastTime);
+            f.draw();
+        }
     }
 
     lastTime = time;
