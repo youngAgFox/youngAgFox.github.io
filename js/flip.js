14e8b26571ce67fef7c5df6c81732f5f8c9429e7
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2022-05-19 20:18:56 -0500

    Updated project look

---
+++ js/flip.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll(".flip-card-inner");
+for (let c of cards) {
+    c.addEventListener("click", () => c.classList.toggle("flip"));
+}
+
+const fronts = document.querySelectorAll(".flip-card-front");
+let e;
+// Add the flip hint text
+for (let f of fronts) {
+    e = document.createElement("div");
+    e.style = "position: absolute; left: 1ch; bottom: 1ch; opacity: 90%; color: hsl(var(--clr-lightBlue));";
+    e.innerText = "Click to Flip";
+    f.insertBefore(e, f.firstChild);
+}
