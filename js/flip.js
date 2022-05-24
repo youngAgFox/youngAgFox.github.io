0eacb0460bed789f95465fcad4da17e4bdfae048
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2022-05-24 16:28:54 -0500

    improved comments

--- js/flip.js
+++ js/flip.js
@@ -1,4 +1,5 @@
 const cards = document.querySelectorAll(".flip-card");
+// Add the listener to the outer flip-card, and flip the inner container
 for (let c of cards) {
     let inner = c.querySelector(".flip-card-inner");
     c.addEventListener("click", () => inner.classList.toggle("flip"));
@@ -7,7 +8,7 @@ for (let c of cards) {
 
 const fronts = document.querySelectorAll(".flip-card-front");
 let e;
-// Add the flip hint text
+// Add the flip hint text to the front cards
 for (let f of fronts) {
     e = document.createElement("div");
     e.style = "position: absolute; left: 1ch; top: 1ch; opacity: 90%; color: hsl(var(--clr-lightBlue)); font-size: 12px;";
