cf00fa19200ea4ed8a35ae147782a5725819ef4f
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2022-05-24 21:45:46 -0500

    Added form

--- js/email.js
+++
@@ -1,7 +0,0 @@
-function sendEmail(name, email, msg) {
-    const encEmail = encodeURIComponent(email);
-    const encMsg = encodedURIComponent(msg);
-    const encSubject = encodedURIComponent(`Portfolio message from ${name}`);
-    const link = `mailto:${encEmail}?subject=${encSubject}&body=${encMsg}`;
-    window.open(link, '_blank');
-}
