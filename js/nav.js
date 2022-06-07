14741f1f108e78268e666656c6b25071954db17a
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2022-06-06 20:03:46 -0500

    Fixed centering bug for name

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,10 @@
 const body = document.querySelector("body");
 const e = document.createElement("div");
-e.style = "height: 3em; padding: 0 2ch; position: sticky; top: 0; z-index: 1000; justify-content: space-between; align-items: center; gap: 15px; overflow: hidden;";
+e.style = "height: 3em; max-height: 3em; padding: 0 2ch; position: sticky; top: 0; z-index: 1000; justify-content: space-between; align-items: center; gap: 15px; overflow: hidden;";
 e.id = "nav";
 e.classList.add("bg-darkSteel", "fc-white", "fs-300", "flex");
 e.innerHTML = `
-    <a href="/" style="line-height: 1.5em; height: 3em; max-height: 4em; overflow: hidden;">Anthony Segedi <span style="color: hsl(var(--clr-steel));">Portfolio</span></a>
+    <a href="/" style="line-height: 1.5em; max-height: 3em; overflow: hidden;">Anthony Segedi <span style="color: hsl(var(--clr-steel));">Portfolio</span></a>
     <div class="flex fs-200" style="height: 100%; gap: 20px; align-items: center;">
         <a href="/#contact-form">Contact</a>
         <span style="margin: 0 -10px;">|</span>
